e56621b8a1a190dc3ec268501ca13e8d5030f14a
Braulio Moroni C Rodrigues <moroniraziel@gmail.com>
2022-01-23 16:10:54 -0300

    Monitorando Erros com Sentry
    close #70

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    raise ValueError()
     return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
