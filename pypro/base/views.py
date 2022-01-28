ae91da0bc31ad5540da74ef290c152bee41e60dc
Braulio Moroni C Rodrigues <moroniraziel@gmail.com>
2022-01-28 20:57:23 -0300

    Sentry Fix 3
    close #75

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    raise ValueError()
     return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
