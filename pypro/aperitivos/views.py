ba854a62711d4649084e167311d3c7d15af0f727
Braulio Moroni C Rodrigues <moroniraziel@gmail.com>
2022-03-01 18:00:57 -0300

    Contexto de Template
    close #146

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 682944764}
+    return render(request, 'aperitivos/video.html', context={'video':video})
