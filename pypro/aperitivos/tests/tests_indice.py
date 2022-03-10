307629edec5f4e7fa4b635f30dca426391ea9661
Braulio Moroni C Rodrigues <moroniraziel@gmail.com>
2022-03-10 13:53:14 -0300

    Acesso a Método no Template
    close #166

--- pypro/aperitivos/tests/tests_indice.py
+++ pypro/aperitivos/tests/tests_indice.py
@@ -8,8 +8,10 @@ from pypro.django_assertions import assert_contains
 def resp(client):
     return client.get(reverse('aperitivos:indice'))
 
+
 def test_status_code(resp):
     assert resp.status_code == 200
+
 
 @pytest.mark.parametrize(
     'titulo',
@@ -21,5 +23,14 @@ def test_status_code(resp):
 def test_titulo_video(resp, titulo):
     assert_contains(resp, titulo)
 
-# def test_conteudo_video(resp):
-#     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/682944764?h=e6821b7310&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"')
+
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'motivacao',
+        'instalacao-windows'
+    ]
+)
+def test_link_video(resp, slug):
+    video_link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{video_link}"')
