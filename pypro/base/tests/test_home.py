eba85fdabff53410b77301761eb0450c0407e396
Braulio Moroni C Rodrigues <moroniraziel@gmail.com>
2022-03-10 14:30:54 -0300

    Composição de Templates
    close #169

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -5,10 +5,12 @@ from django.urls import reverse
 
 from pypro.django_assertions import assert_contains
 
+
 @pytest.fixture
 def resp(client):
     resp = client.get(reverse('base:home'))
     return resp
+
 
 def test_status_code(resp):
     assert resp.status_code == 200
@@ -17,5 +19,10 @@ def test_status_code(resp):
 def test_title(resp):
     assert_contains(resp, '<title>Python Pro - Home</title>')
 
+
 def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
+
+
+def test_email_link(resp):
+    assert_contains(resp, 'href="mailto:ramalho@python.com.br"')
