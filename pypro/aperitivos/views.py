b158fd40cd679d05d0486f76d7baf1087918440f
Braulio Moroni C Rodrigues <moroniraziel@gmail.com>
2022-04-09 20:55:26 -0300

    ERRO modelos do Django

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,18 +1,19 @@
+from django.db import models
 from django.shortcuts import render
 from django.urls import reverse
 
 
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.vimeo_id = vimeo_id
+class Video(models.Model):
+    slug = models.CharField(max_length=32)
+    titulo = models.CharField(max_length=32)
+    vimeo_id = models.CharField(max_length=32)
+
     def get_absolute_url(self):
         return reverse('aperitivos:video', args=(self.slug,))
 
 videos = [
-    Video('motivacao', 'Video Aperitivo: Motivação',  682944764),
-    Video('instalacao-windows', 'Instalação Windows', 682947239),
+    Video(slug='motivacao', titulo='Video Aperitivo: Motivação',  vimeo_id='682944764'),
+    Video(slug='instalacao-windows', titulo='Instalação Windows', vimeo_id='682947239'),
 ]
 
 videos_dct = {v.slug: v for v in videos}
